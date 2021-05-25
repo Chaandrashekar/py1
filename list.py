629231de9869cc5ab5af1d19be6bdf5f101c4efa
Chandrashekar N <83531748+Chaandrashekar@users.noreply.github.com>
2021-05-25 15:12:14 +0530

    Add files via upload
    this list program

---
+++ list.py
@@ -0,0 +1,17 @@
+my_list=[]
+print(my_list)
+my_list_2=list()
+print(my_list_2)
+#add elements to my list
+my_list=[1,2,3,'CSK','RCB','Python']
+my_list_2=list([3.16,'cool','fun'])
+print(my_list)
+print(my_list_2)
+#accessing elements of the list
+my_list=[1,2,3,'CSK','RCB','Python']
+print(my_list[:])#this prints all the elements in the list we can also type print(my_list)
+print(my_list[3])#this print the elements in the 3rd index \
+print(my_list[0:4])#it prints elements from 0 to 3 in index and leaves the other index's elements so to say it in a simple way its n-1 elements are repersented in the output screen
+print(my_list[0:5:2])#access the elements from 0 to 4 is jumped and 2 elements instead of 1
+print(my_list[3][4])
+
